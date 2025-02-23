94c34a3272ece0707c5ba515c8fc755d4b1ddb7c
bormoge <192043292+bormoge@users.noreply.github.com>
2025-02-23 11:49:08 -0700

    Modularizing my emacs config

---
+++ my-emacs-config/functions/copy-the-entire-line.el
@@ -0,0 +1,24 @@
+;; Funtion for copying a line
+(defun copy-the-entire-line ()
+  "Execute the functions `move-beginning-of-line', `set-mark', `move-end-of-line', and `kill-ring-save'.\n
+Copies the entire line of text."
+  (interactive)
+  (move-beginning-of-line 1)
+  (set-mark (point))
+  ;; (next-line)
+  (move-end-of-line 1)
+  (kill-ring-save (point) (mark)))
+
+(global-set-key (kbd "M-s-w") 'copy-the-entire-line)
+
+;; Funtion for copying and pasting a line
+(defun copy-paste-the-entire-line ()
+  "Execute the functions `copy-the-entire-line', `open-line', `next-line', and `yank'.\n
+Copies the entire line of text, creates a new line, and pastes the copied line in the new line."
+  (interactive)
+  (copy-the-entire-line)
+  (open-line 1)
+  (next-line)
+  (yank))
+
+(global-set-key (kbd "M-s-y") 'copy-paste-the-entire-line)
