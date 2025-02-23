e6027a498b43ef17ed2b43173aa8ce71661b3b1a
bormoge <192043292+bormoge@users.noreply.github.com>
2025-02-23 11:49:08 -0700

    Modularizing my emacs config

--- init.el
+++ init.el
@@ -11,44 +11,18 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; Maximize Emacs on start
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(defconst user-init-dir
+  (cond ((boundp 'user-emacs-directory)
+         user-emacs-directory)
+        ((boundp 'user-init-directory)
+         user-init-directory)
+        (t "~/.emacs.d/")))
 
-;; Change focus of windows and frames using Alt+o and Alt+shift+o
-;;(global-set-key (kbd "M-o") #'other-window)
-;;(global-set-key (kbd "M-O") #'other-frame)
-(define-key (current-global-map) (kbd "H-<left>") 'windmove-left)
-(define-key (current-global-map) (kbd "H-<right>") 'windmove-right)
-(define-key (current-global-map) (kbd "H-<up>") 'windmove-up)
-(define-key (current-global-map) (kbd "H-<down>") 'windmove-down)
 
-;; Delete duplicate lines using ALT + SUPER (normally Windows Key) + <backspace>
-(define-key (current-global-map) (kbd "M-s-<backspace>") 'delete-duplicate-lines)
+(defun load-user-file (file)
+  "Load a file in current user's configuration directory"
+  (interactive "f")
+  (load-file (expand-file-name file user-init-dir)))
 
-;; Show number of the lines
-(global-display-line-numbers-mode)
-
-;; Funtion for copying a line
-(defun copy-the-entire-line ()
-  "Execute the functions `move-beginning-of-line', `set-mark', `move-end-of-line', and `kill-ring-save'.\n
-Copies the entire line of text."
-  (interactive)
-  (move-beginning-of-line 1)
-  (set-mark (point))
-  ;; (next-line)
-  (move-end-of-line 1)
-  (kill-ring-save (point) (mark)))
-
-(global-set-key (kbd "M-s-w") 'copy-the-entire-line)
-
-;; Funtion for copying and pasting a line
-(defun copy-paste-the-entire-line ()
-  "Execute the functions `copy-the-entire-line', `open-line', `next-line', and `yank'.\n
-Copies the entire line of text, creates a new line, and pastes the copied line in the new line."
-  (interactive)
-  (copy-the-entire-line)
-  (open-line 1)
-  (next-line)
-  (yank))
-
-(global-set-key (kbd "M-s-y") 'copy-paste-the-entire-line)
+(load-user-file "my-emacs-config/my-emacs-config.el")
+(load-user-file "my-emacs-config/functions/copy-the-entire-line.el")
---
+++ my-emacs-config/my-emacs-config.el
@@ -0,0 +1,16 @@
+;; Maximize Emacs on start
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Change focus of windows and frames using Alt+o and Alt+shift+o
+;;(global-set-key (kbd "M-o") #'other-window)
+;;(global-set-key (kbd "M-O") #'other-frame)
+(define-key (current-global-map) (kbd "H-<left>") 'windmove-left)
+(define-key (current-global-map) (kbd "H-<right>") 'windmove-right)
+(define-key (current-global-map) (kbd "H-<up>") 'windmove-up)
+(define-key (current-global-map) (kbd "H-<down>") 'windmove-down)
+
+;; Delete duplicate lines using ALT + SUPER (normally Windows Key) + <backspace>
+(define-key (current-global-map) (kbd "M-s-<backspace>") 'delete-duplicate-lines)
+
+;; Show number of the lines
+(global-display-line-numbers-mode)
