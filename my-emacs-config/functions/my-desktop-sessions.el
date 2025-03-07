4543e1c519f88da5e5ee4ff64d15c7c3dfa99587
bormoge <192043292+bormoge@users.noreply.github.com>
2025-03-07 12:22:32 -0700

    Adding new buffer that shows saved sessions and making it default startup.

--- my-emacs-config/functions/my-desktop-sessions.el
+++ my-emacs-config/functions/my-desktop-sessions.el
@@ -1,3 +1,6 @@
+;;; -*- lexical-binding: t; -*-
+;; Necessary for my-desktop-link-text
+
 (defvar my-desktop-session-dir
   (concat (getenv "HOME") "/.emacs.d/desktop-sessions/")
   "Set a directory to save desktop sessions.")
@@ -21,6 +24,67 @@
   (desktop-read (concat my-desktop-session-dir name)))
 
 (defun my-desktop-get-session-name (prompt)
+  "Get the session using its name."
   (completing-read prompt (and (file-exists-p my-desktop-session-dir)
                                (directory-files my-desktop-session-dir))
                    nil nil nil my-desktop-session-hist))
+
+;; I'll see what I do with this
+
+;; (defun my-desktop-list-sessions ()
+;;   "Get a list of all sessions."
+;;   (interactive)
+;;   (switch-to-buffer "*List of sessions*")
+;;   (erase-buffer)
+;;   (insert "Sessions saved:\n\n")
+;;   (let ((dir-list (directory-files my-desktop-session-dir)))
+;;       (dolist (value dir-list)
+;; 	(unless (member value '("." ".."))
+;; 	  ;;(insert (format "%s\n" value))
+;; 	  (my-desktop-link-text (format "%s" value))
+;; 	  (insert "\n")
+;; 	  )
+;; 	)
+;;       )
+;;   (read-only-mode 1)
+;;   )
+
+(defun my-desktop-buffer-list-sessions ()
+  "Get a list of all sessions."
+  (let ((session-buffer (generate-new-buffer "*List of sessions*")))
+    (with-current-buffer session-buffer
+      (erase-buffer)
+      (insert "Sessions saved:\n\n")
+      (let ((dir-list (directory-files my-desktop-session-dir)))
+        (dolist (value dir-list)
+          (unless (member value '("." ".."))
+            (my-desktop-link-text (format "%s" value))
+            (insert "\n"))))
+      (read-only-mode 1)
+      ;; (setq-local
+      ;;  split-width-threshold 0
+      ;;  split-height-threshold nil)
+    session-buffer)))
+
+(defun my-desktop-open-buffer-list ()
+  "Directly open the buffer to get a list of all sessions"
+  (interactive)
+  (switch-to-buffer (my-desktop-buffer-list-sessions))
+  )
+
+(defun my-desktop-open-buffer-list-right ()
+  "Get a list of all sessions in a split window."
+  (interactive)
+  (split-window-right)
+  (other-window 1)
+  (my-desktop-open-buffer-list)
+  (other-window 1)
+  )
+
+(defun my-desktop-link-text (text-to-link)
+  "Put a link in the text"
+  (insert-text-button text-to-link
+		      'action (let ((linked-text text-to-link))
+				(lambda (x) (my-desktop-read linked-text)))
+		      'follow-link t)
+  )
--- my-emacs-config/my-emacs-config.el
+++ my-emacs-config/my-emacs-config.el
@@ -1,3 +1,17 @@
+;; Remove startup screen
+(setq inhibit-startup-screen t)
+;;(setq initial-buffer-choice 'my-desktop-buffer-list-sessions)
+
+;; Replace boring scratch with custom buffer that contains links to sessions (~/.emacs.d/desktop-sessions) using my-desktop-sessions.el
+(defun check-if-file-at-startup ()
+  "Check if a file is being opened at startup."
+  (if (or (buffer-file-name) load-file-name)
+      (message "A file is opened, skipping desktop sessions buffer.")
+    (my-desktop-open-buffer-list)))
+
+;; Run the check after Emacs initialization
+(add-hook 'emacs-startup-hook 'check-if-file-at-startup)
+
 ;; Maximize Emacs on start
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
@@ -69,8 +83,11 @@
 ;; Enable Horizontal Scroll Bar
 (horizontal-scroll-bar-mode 1)
 
-;; Make it so you replace a selected area with typed text
+;; Replace a selected area with typed text
 (delete-selection-mode 1)
+
+;; Not sure if I should add lexical binding by default
+;;(setq lexical-binding t)
 
 ;; Change syntax behavior
 ;; (modify-syntax-entry ?@ "w")
